9700da0c8f5c20b4f3bed6f62355c330792825d2
Claudia Pierson <c172@umbc.edu>
2020-04-21 04:32:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -29,3 +29,23 @@ class Dog
   end
 
 end
+
+
+# Dog
+#   .new
+#     initializes with one argument - a name
+#   class variables
+#     @@all
+#       has a class variable, @@all, the points to an array
+#   class methods
+#     .all
+#       is a class method returns all dog instances
+#     .clear_all
+#       is a class method that empties the @@all array of all existing dogs
+#     .print_all
+#       is a class method that puts out the name of each dog to the terminal
+#   #save
+#     adds this dog instance to the @@all array when called
+#     gets called inside initialize when a new Dog is created
+#   #name
+#     has a name
